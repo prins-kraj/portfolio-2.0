df95fdc0d7fde6fdc62a0751fe6cda6ae4eaf23f
Prince Kumar <princesansaraj@gmail.com>
2025-10-02 02:17:50 +0530

    CORS issue resolved

--- backend/server.js
+++ backend/server.js
@@ -18,8 +18,14 @@ app.use((req, res, next) => {
 // Security middleware
 app.use(helmet());
 app.use(cors({
-  origin: process.env.CORS_ORIGIN || 'http://localhost:5173',
-  credentials: true
+  origin: [
+    process.env.CORS_ORIGIN || 'http://localhost:5173',
+    'http://localhost:5173',
+    'http://localhost:5173/'
+  ],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 // Rate limiting - More restrictive for contact form
